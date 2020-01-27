7c8dd0ae6cc447479f231df246716d3cfc4fdc75
Peter <pc.dean@icloud.com>
2020-01-27 14:47:13 +0000

    created balance atribute

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,2 +1,12 @@
 class Oystercard
+
+  attr_reader :balance
+
+  def initialize(balance = 0)
+    self.balance = balance
+  end
+
+  private
+  attr_writer :balance
+
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,4 +1,9 @@
 require 'oystercard.rb'
 
 describe Oystercard do
+
+  it 'oystercard.balance should return 0' do
+    expect(subject.balance).to eq(0)
+  end
+
 end
